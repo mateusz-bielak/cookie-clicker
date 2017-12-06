7342283c2a4414a6c147ab1107b842d0a7de91af
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-06 21:50:43 +0100

    Styling settings and base styles

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CookiePanel from './components/CookiePanel';
+
 import '../node_modules/normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const test = (
   <div>

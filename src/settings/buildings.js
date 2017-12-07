1b829e5afcf9cacd8df4e2f6a4f661470b8c2b4a
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-07 21:45:56 +0100

    Available buildings parameters

---
+++ src/settings/buildings.js
@@ -0,0 +1,30 @@
+const buildings = [
+  {
+    name: 'Cursor',
+    description: 'Autoclicks once every 10 seconds.',
+    initialCost: 15,
+    productionPerSecond: 0.1,
+  }, {
+    name: 'Grandma',
+    description: 'A nice grandma to bake more cookies.',
+    initialCost: 100,
+    productionPerSecond: 1,
+  }, {
+    name: 'Farm',
+    description: 'Grows cookie plants from cookie seeds.',
+    initialCost: 1100,
+    productionPerSecond: 8,
+  }, {
+    name: 'Mine',
+    description: 'Mines out cookie dough and chocolate chips.',
+    initialCost: 12000,
+    productionPerSecond: 47,
+  }, {
+    name: 'Factory',
+    description: 'Produces large quantities of cookies.',
+    initialCost: 130000,
+    productionPerSecond: 260,
+  },
+];
+
+export default buildings;

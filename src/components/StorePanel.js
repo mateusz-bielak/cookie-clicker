dc30641000e8421e9ade871f7f3b459e2df21b3b
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-07 14:38:05 +0100

    Store panel scaffolding

---
+++ src/components/StorePanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const StorePanel = () => (
+  <div className="store_panel">
+    <section>
+      <h1>Store</h1>
+    </section>
+    <section>
+      <p>Upgrades</p>
+    </section>
+    <section>
+      <p>Buildings</p>
+    </section>
+  </div>
+);
+
+export default StorePanel;

a9be4ef7954d4596fa9d79853b6f98ccc7071798
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-13 22:30:11 +0100

    Cookie animation on click

--- src/components/CookiePanel.js
+++ src/components/CookiePanel.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CookiePanel = props => (
-  <div className="cookie_panel">
-    <input
-      className="cookie_panel__header"
-      type="text"
-      defaultValue="SkyGate Ticket"
-    />
-    <div className="cookie_panel__stats">
-      <p>{props.cookiesAmount.toFixed(0)} cookies</p>
-      <p>per second: {props.cookiesPerSecond.toFixed(1)}</p>
-    </div>
-    <button
-      className="cookie_panel__button"
-      onClick={props.addCookie}
-    />
-  </div>
-);
+class CookiePanel extends React.Component {
+  cookieBouncing = () => {
+    this.props.addCookie();
+    document.querySelector('.cookie_panel__button').classList.toggle('cookie_panel__button--bounce');
+  }
+
+  render() {
+    return (
+      <div className="cookie_panel">
+        <input
+          className="cookie_panel__header"
+          type="text"
+          defaultValue="SkyGate Ticket"
+        />
+        <div className="cookie_panel__stats">
+          <p>{this.props.cookiesAmount.toFixed(0)} cookies</p>
+          <p>per second: {this.props.cookiesPerSecond.toFixed(1)}</p>
+        </div>
+        <button
+          className="cookie_panel__button"
+          onClick={this.cookieBouncing}
+        />
+      </div>
+    );
+  }
+}
 
 CookiePanel.propTypes = {
   cookiesAmount: PropTypes.number.isRequired,

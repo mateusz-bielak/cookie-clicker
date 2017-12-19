e675324bf4c1098a68c76da9395d7f51201fce89
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-19 12:40:33 +0100

    Updating and getting methods for IndexedDB data

---
+++ src/indexedDB/methods.js
@@ -0,0 +1,26 @@
+import databaseStoreName from './setup';
+
+export const getFromDatabase = (database, key) => {
+  const transaction = database
+    .transaction(databaseStoreName);
+  const store = transaction.objectStore(databaseStoreName);
+  const request = store.get(key);
+
+  request.onsuccess = (event) => {
+    console.log(event.target.result);
+  };
+};
+
+export const updateDatabase = (database, key, value) => {
+  const transaction = database
+    .transaction(databaseStoreName, 'readwrite');
+  const store = transaction.objectStore(databaseStoreName);
+  const request = store.get(key);
+
+  request.onsuccess = (event) => {
+    let data = event.target.result;
+    data = value;
+
+    store.put(data, key);
+  };
+};

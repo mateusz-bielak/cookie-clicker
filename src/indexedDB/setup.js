79874554a0988d82ee9a26f6ffa555486714f15d
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-19 12:42:23 +0100

    Setup code refactoring & updating cookies amount every 30 seconds

--- src/indexedDB/setup.js
+++ src/indexedDB/setup.js
@@ -1,10 +1,30 @@
+import { getFromDatabase, updateDatabase } from './methods';
+
+let database;
 const databaseName = 'cookie-game-database';
 const databaseVersion = 1;
 const databaseStoreName = 'user-data';
 
+// indexedDB.deleteDatabase(databaseName);
+
 const openDatabase = indexedDB.open(databaseName, databaseVersion);
 
 openDatabase.onupgradeneeded = () => {
-  const database = openDatabase.result;
-  database.createObjectStore(databaseStoreName, { autoIncrement: true });
+  const databaseToUpgrade = openDatabase.result;
+  databaseToUpgrade.createObjectStore(databaseStoreName);
 };
+
+openDatabase.onsuccess = (event) => {
+  database = event.target.result;
+};
+
+setInterval(() => {
+  const cookiesAmount = document
+    .querySelector('.cookie_panel__stats>p')
+    .textContent
+    .split(' ')[0];
+
+  updateDatabase(database, 'cookiesAmount', cookiesAmount);
+}, 30000);
+
+export default databaseStoreName;
